bf0db445f885ee392d09737fa91a1e8cfac2a9cd
Dillon Nguyen <dillonkn@stanford.edu>
2024-04-20 11:46:51 -0700

    Created backend subdir and postgres schema

---
+++ backend/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to Next.js</h1>
+      {/* Add your content here */}
+    </div>
+  );
+};
+
+export default HomePage;
